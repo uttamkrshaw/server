9481b48d69d677d7efa70acdc76b8a0de946e51e
uttamkrshaw <uttamkr5599@gmail.com>
2023-04-21 00:43:51 +0530

    test

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const data = require('./db.json');
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(data);
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-server.listen(port, () => console.log(`JSON Server is running on port ${port}`));
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
